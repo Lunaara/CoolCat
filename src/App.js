48b047fc18827a51eb9eb04268db37e36e4bfed5
Van <sedansvan@gmail.com>
2019-01-18 15:06:06 -0800

    ported to react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-restricted-globals */
 import React, { Component } from 'react';
-import $ from 'jquery';
 import Nav from './components/Nav';
 import Header from './components/Header';
 import About from './components/About';
@@ -11,6 +10,45 @@ import Footer from './components/Footer';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    window.window.$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = window.$(this.hash);
+        target = target.length ? target : window.$('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          window.$('html, body').animate({
+            scrollTop: (target.offset().top - 70)
+          }, 1000, "easeInOutExpo");
+          return false;
+        }
+      }
+    });
+  
+    // Closes responsive menu when a scroll trigger link is clicked
+    window.$('.js-scroll-trigger').click(function() {
+      window.$('.navbar-collapse').collapse('hide');
+    });
+  
+    // Activate scrollspy to add active class to navbar items on scroll
+    window.$('body').scrollspy({
+      target: '#mainNav',
+      offset: 100
+    });
+  
+    // Collapse Navbar
+    var navbarCollapse = function() {
+      if (window.$("#mainNav").offset().top > 100) {
+        window.$("#mainNav").addClass("navbar-shrink");
+      } else {
+        window.$("#mainNav").removeClass("navbar-shrink");
+      }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    window.$(window).scroll(navbarCollapse);
+  }
+
   render() {
     return (
       <div className="App" id="page-top">
